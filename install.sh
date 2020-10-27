27fe6f71f2407d9a9430aabd3e63a66371acf346
maade69 <p1nchp0d@bk.ru>
2020-10-27 17:28:16 +0300

    why not

--- install.sh
+++ install.sh
@@ -31,11 +31,11 @@ echo -E "                                                              "
 sleep 5
 clear
 
-pkg update && pkg upgrade -y
-pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
+apt update && apt upgrade -y
+apt install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
-git clone https://github.com/mkaraniya/OpenUserBot.git
-cd OpenUserBot
+git clone https://github.com/sahyam2019/oub-remix.git
+cd oub-remix
 
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
@@ -50,7 +50,7 @@ createuser botuser
 
 cd ..
 echo "pg_ctl -D $PREFIX/var/lib/postgresql start" > startbot.sh
-echo "cd OpenUserBot" >> startbot.sh
+echo "cd oub-remix" >> startbot.sh
 echo "python3 -m userbot" >> startbot.sh
 chmod 755 startbot.sh
 
